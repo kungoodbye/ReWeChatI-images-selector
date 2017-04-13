3c72dca53b056cf1ece77ef0586aaf00da54f89d
kungoodbye <55443128@qq.com>
2017-04-13 10:45:48 +0800

    更新选择结果

--- demo/src/main/java/com/example/demo/MainActivity.java
+++ demo/src/main/java/com/example/demo/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.rewechati_images_selector.ImagesSelectorActivity;
 import com.example.rewechati_images_selector.SelectorSettings;
@@ -13,6 +14,8 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private ArrayList<String> mResults = new ArrayList<>();
+    private static final int REQUEST_CODE = 732;
+    private TextView tvResults;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,16 +30,36 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this, ImagesSelectorActivity.class);
                 // 最大图片选择
                 intent.putExtra(SelectorSettings.SELECTOR_MAX_IMAGE_NUMBER, 5);
-                // min size of image which will be shown; to filter tiny images (mainly icons)
+                //显示最小尺寸的图像;过滤小图像 (mainly icons)
                 intent.putExtra(SelectorSettings.SELECTOR_MIN_IMAGE_SIZE, 100000);
                 // 是否显示摄像头
                 intent.putExtra(SelectorSettings.SELECTOR_SHOW_CAMERA, false);
-                // pass current selected images as the initial value
+                // 通过当前选中的图像作为初始值
                 intent.putStringArrayListExtra(SelectorSettings.SELECTOR_INITIAL_SELECTED_LIST, mResults);
 //                 start the selector
-                startActivity(intent);
+                startActivityForResult(intent, REQUEST_CODE);
             }
         });
+        tvResults = (TextView) findViewById(R.id.results);
     }
-    //测试
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // get selected images from selector
+        if(requestCode == REQUEST_CODE) {
+            if(resultCode == RESULT_OK) {
+                mResults = data.getStringArrayListExtra(SelectorSettings.SELECTOR_RESULTS);
+                assert mResults != null;
+
+                // show results in textview
+                StringBuilder sb = new StringBuilder();
+                sb.append(String.format("共选了 %d 张图片:", mResults.size())).append("\n");
+                for(String result : mResults) {
+                    sb.append(result).append("\n");
+                }
+                tvResults.setText(sb.toString());
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 }
